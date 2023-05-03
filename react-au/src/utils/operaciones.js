061fae5ea15ec689025b26ca2641d42530cd8edf
Angel Ariel <anllelo13.plaza@gmai.com>
2023-05-02 22:05:49 -0600

    operaciones fix
    se corrigio el descuento  de 1% cuando se aplica el entero, este debe dar el precio total del boleto

--- react-au/src/utils/operaciones.js
+++ react-au/src/utils/operaciones.js
@@ -15,17 +15,26 @@ export const operacionesFunc = (newBoleto, data) => {
       }
     }
   }
-  let totalmodel = precioRuta * newBoleto.num_boletos;
   let total = precioRuta;
-
-  let total_de_venta = total - total * (newBoleto.descuento / 100);
-  let total_de_venta_model = total - total * (newBoleto.descuento / 100);
-  total_de_venta_model =
-    Math.ceil(total_de_venta_model) * newBoleto.num_boletos;
-  newBoleto.totalventa = Math.ceil(total_de_venta);
-  newBoleto.totalventamodel = Math.ceil(total_de_venta_model);
-  newBoleto.num_boletos_model = newBoleto.num_boletos;
-  newBoleto.total = total;
+  if (newBoleto.descuento === 1) {
+    let total_de_venta = total;
+    let total_de_venta_model = total;
+    total_de_venta_model =
+      Math.ceil(total_de_venta_model) * newBoleto.num_boletos;
+    newBoleto.totalventa = Math.ceil(total_de_venta);
+    newBoleto.totalventamodel = Math.ceil(total_de_venta_model);
+    newBoleto.num_boletos_model = newBoleto.num_boletos;
+    newBoleto.total = total;
+  } else {
+    let total_de_venta = total - total * (newBoleto.descuento / 100);
+    let total_de_venta_model = total - total * (newBoleto.descuento / 100);
+    total_de_venta_model =
+      Math.ceil(total_de_venta_model) * newBoleto.num_boletos;
+    newBoleto.totalventa = Math.ceil(total_de_venta);
+    newBoleto.totalventamodel = Math.ceil(total_de_venta_model);
+    newBoleto.num_boletos_model = newBoleto.num_boletos;
+    newBoleto.total = total;
+  }
 
   // devolver la variable modificada
   return { ...newBoleto, total }; // devolvemos un nuevo objeto con la propiedad "total" actualizada
