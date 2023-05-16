efe0a374f023f1a3b22a707aac145c48ec637bb4
ANGEL ARIEL <75581459+ANGELARIELPLAZA@users.noreply.github.com>
2023-05-16 07:48:03 -0600

    update

---
+++ report/models/VentaModel.js
@@ -0,0 +1,52 @@
+const mongoose = require("mongoose");
+const moment = require("moment-timezone");
+const Schema = mongoose.Schema;
+
+let ventaSchema = new Schema({
+  destino_code: {
+    type: String,
+    required: true,
+  },
+  num_boletos: {
+    type: String,
+    required: true,
+  },
+  descuento: {
+    type: Number,
+    required: true,
+  },
+  token: {
+    type: String,
+    required: true,
+  },
+  vendedor: {
+    type: String,
+    required: true,
+  },
+  nombre_ruta: {
+    type: String,
+    required: true,
+  },
+  caja: {
+    type: Number,
+    required: true,
+  },
+  total: {
+    type: Number,
+    required: true,
+  },
+  totalventa: {
+    type: Number,
+    required: true,
+  },
+  fecha: {
+    type: String,
+    required: true,
+  },
+  hora: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("Venta", ventaSchema, "ventas");
